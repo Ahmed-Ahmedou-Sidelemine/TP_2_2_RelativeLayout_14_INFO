de47166177e19052eb3447839a238d57362f3f04
Ahmed Ahmedou Sid'elemine <ahmedisi804@gmail.com>
2020-07-05 14:53:05 +0200

    Initial commit

---
+++ app/src/main/java/mr/cset/tp_2_2_relativelayout_14/MainActivity.java
@@ -0,0 +1,14 @@
+package mr.cset.tp_2_2_relativelayout_14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
